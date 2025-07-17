75c975e7ae0c3562970db50eb68b8b08468a4094
unknown <raresneculae@gmail.com>
2025-07-17 06:41:53 +0300

    chore: Remove start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status.
-set -o errexit
-
-echo "--- Installing dependencies using python -m pip ---"
-python -m pip install -r requirements.txt
-
-echo "--- Starting server with python -m gunicorn ---"
-python -m gunicorn --config gunicorn_config.py app:app
